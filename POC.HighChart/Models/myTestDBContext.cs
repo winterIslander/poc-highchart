146b9e630a0c3ae54ae11e9b3a68a610138b1480
winterislander <winterislander>
2019-02-21 13:26:14 -0600

    updating connection string

--- POC.HighChart/Models/myTestDBContext.cs
+++ POC.HighChart/Models/myTestDBContext.cs
@@ -13,7 +13,7 @@ namespace IPLPollDemo.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=IPLPoll;Persist Security Info=True;Integrated Security = true");
+                optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=IPLPoll;Persist Security Info=True;Integrated Security = true");
             }
         }
 
